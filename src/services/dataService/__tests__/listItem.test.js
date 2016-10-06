d969614bc08c05ee2c9d00e0d5abfc7b072be36b
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-06 09:35:36 -0700

    created separate test files for user,list,listitem

---
+++ src/services/dataService/__tests__/listItem.test.js
@@ -0,0 +1,3 @@
+const test = require('../../../test')
+const seedData = require('../../../test/seedData')
+const {getUser, getList, findLists} = require('../../dataService')
---
+++ src/services/dataService/__tests__/user.test.js
@@ -0,0 +1,14 @@
+const test = require('../../../test')
+const seedData = require('../../../test/seedData')
+const {getUser, getList, findLists} = require('../../dataService')
+
+test('dataService', function(t) {
+  return seedData().then(() => {
+    t.test('Get user test', function (st) {
+      getUser(1).then(function (user) {
+        st.equal(user.id, 1, 'Fuck yeah!' )
+        st.end()
+      })
+    })
+  })
+})
