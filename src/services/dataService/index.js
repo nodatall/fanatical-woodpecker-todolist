0348c35972c071a7454f14d912636a0c579dbe09
Sylvan Swierkosz <sswierkosz@gmail.com>
2016-10-07 13:17:17 -0700

    things and stuff

--- src/services/dataService/index.js
+++ src/services/dataService/index.js
@@ -1,5 +1,5 @@
 const lists = require('./lists')
 const listItems = require('./listItems')
-const listItems = require('./user')
+const user = require('./user')
 
 module.exports = Object.assign({}, lists, listItems, user)
